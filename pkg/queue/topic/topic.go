de7c74ebb7c5d1db6a77a7539ee6cc60ddcca220
KritAsawaniramol <krit.as@ku.th>
2024-10-31 19:15:37 +0700

    update create topic

--- pkg/queue/topic/topic.go
+++ pkg/queue/topic/topic.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/IBM/sarama"
+	"github.com/kritAsawaniramol/book-store/config"
 )
 
 func main() {
-	brokers := []string{"localhost:9092"}
+	cfg := config.LoadConfig(func() string {
+		if len(os.Args) < 2 {
+			log.Fatal("Error: .env path is required")
+		}
+		return os.Args[1]
+	}())
+	// brokers := []string{"localhost:9092"}
+	brokers := []string{cfg.Kafka.Url}
 	config := sarama.NewConfig()
 	config.Version = sarama.V3_6_0_0
 	config.Producer.Return.Successes = true
@@ -28,7 +37,6 @@ func main() {
 		// partition assignment or using the default replication factor.
 		ReplicationFactor: 1,
 	}
-
 
 	topicName := []string{"order", "user", "shelf"}
 
